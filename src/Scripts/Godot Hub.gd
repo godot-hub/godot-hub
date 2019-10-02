ea5f075877df65758c0a95cab1adf55538b651b4
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2019-10-02 20:50:48 +0200

    Close #21 - :alembic: use HTTPRequest node to call the releases url

--- src/Scripts/Godot Hub.gd
+++ src/Scripts/Godot Hub.gd
@@ -1,13 +1,9 @@
 extends Control
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$HTTPRequest.request("https://downloads.tuxfamily.org/godotengine/")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_HTTPRequest_request_completed(result, response_code, headers, body):
+	var data = body.get_string_from_utf8()
+	$RichTextLabel.text = data
