c6d426f9f85339b4f20b13dd01de8c8aa38df6cd
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2019-10-05 20:53:48 +0200

    Close #25 - :fire: empty the project from v0.1.0 with its related files & directories

--- src/Scripts/Godot Hub.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	$HTTPRequest.request("https://downloads.tuxfamily.org/godotengine/")
-
-func _on_HTTPRequest_request_completed(result, response_code, headers, body):
-	var data = body.get_string_from_utf8()
-	$RichTextLabel.text = data
