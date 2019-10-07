e013b9cc92321f4a4d7a7e470d8eb065c95c6fa0
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2019-10-08 00:41:11 +0200

    Close #44 - :tada: add missing index.js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+const Index = () => (
+    <>
+        <h1>Godot Hub</h1>
+    </>
+);
+
+render(
+    <Index />,
+    document.querySelector("#godot-hub")
+);
