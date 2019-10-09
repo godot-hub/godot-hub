f10fcd9721e10f9eb0d0fd5258235031430f02b0
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2019-10-10 01:25:51 +0200

    Close #56 - :tada: connect react to redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store/store'
 
 const Index = () => (
-    <>
+    <Provider store={store}>
         <h1>Godot Hub</h1>
-    </>
+    </Provider>
 );
 
 render(
