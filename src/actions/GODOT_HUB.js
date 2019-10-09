83772c6728ab58f3ebbc50f02a1fc8a4c2414592
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2019-10-10 01:04:36 +0200

    Close #53 - :tada: add actions directory

---
+++ src/actions/GODOT_HUB.js
@@ -0,0 +1,10 @@
+import GODOT_HUB_TYPE from '../types/GODOT_HUB_TYPE';
+
+const GODOT_HUB = () => ({
+    type: GODOT_HUB_TYPE,
+    payload: {
+        project: 'Godot Hub'
+    }
+});
+
+export default GODOT_HUB;
