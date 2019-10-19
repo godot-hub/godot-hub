2726dc6df90a5b112479c913c574d014a88bed2c
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2019-10-20 01:42:18 +0200

    Close #71 - :wrench: configure jest to use jest-fetch-mock

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,6 @@ import Adapter from 'enzyme-adapter-react-16';
 
 // configure enzyme to work with jest
 configure({ adapter: new Adapter() });
+
+// configure jest to use jest-fetch-mock
+global.fetch = require('jest-fetch-mock');
