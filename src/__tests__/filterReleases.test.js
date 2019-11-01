ad780b1c489b0f21835fed69275453995e2ace5b
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2019-11-01 21:07:30 +0200

    Close #73 - :white_check_mark: pass filterReleases helper's tests

--- src/__tests__/filterReleases.test.js
+++ src/__tests__/filterReleases.test.js
@@ -1,5 +1,9 @@
+import filterReleases from '../helpers/filterReleases';
+const releases = require('../__data__/releases.json');
+const filteredReleasesData =  require('../__data__/filteredReleases.json');
+
 describe('filterReleases()', () => {
     it('should filter godot releases', () => {
-        expect(true).toBeFalsy();
+        expect(filterReleases(releases)).toEqual(filteredReleasesData);
     });
 });
