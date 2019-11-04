7c28ad0d3e278dc09db8734bb5eaf60708ed5c4f
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2019-11-04 16:31:52 +0200

    Close #69 - :white_check_mark: pass sortReleases helper's tests

--- src/__tests__/sortReleases.test.js
+++ src/__tests__/sortReleases.test.js
@@ -1,5 +1,9 @@
+const filteredReleases = require('../__data__/filteredReleases.json');
+const sortedReleases = require('../__data__/sortedReleases.json');
+import sortReleases from '../helpers/sortReleases';
+
 describe('sortReleases()', () => {
     it('should sort godot releases', () => {
-        expect(true).toBeFalsy();
+        expect(sortReleases(filteredReleases)).toEqual(sortedReleases);
     });
 });
