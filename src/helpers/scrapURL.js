5a0955263c9b212f9e9cd322f24e34d4510bb3b8
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2019-12-07 18:13:20 +0200

    Close #82 - :tada: create scrapURL

---
+++ src/helpers/scrapURL.js
@@ -0,0 +1,8 @@
+const { ipcRenderer } = require('electron')
+
+// scrap URL to get important URLs related to a specific godot release
+const scrapURL = (URL) => {
+  ipcRenderer.send('scrapURL-request', URL)
+}
+
+export default scrapURL
