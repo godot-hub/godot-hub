aaedcd3e00704c4c69619a556ff0645f8a414d46
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2019-12-15 14:25:48 +0200

    Close #93 - :tada: create getMonoURL

---
+++ src/helpers/getMonoURL.js
@@ -0,0 +1,31 @@
+const { ipcRenderer } = require('electron');
+
+// return the full url of a mono version based on its godot version, OS and arch
+const getMonoURL = (url, OS) => {
+  console.log(`mono url: ${url}`);
+  console.log(`mono OS info: ${OS}`);
+
+  const monoRequestURL = `${url}mono/`;
+
+  ipcRenderer.send('getMonoURL-request', { url: monoRequestURL, OS });
+
+  ipcRenderer.on('getMonoURL-response', (event, arg) => {
+    const { data, url } = arg;
+    console.log(`getMonoURL-response: ${data}, ${url}`);
+    // parsing DOM and getting required elements for releases
+    const parser = new DOMParser();
+    const elements = parser.parseFromString(data, 'application/xhtml+xml');
+    const listOfReleases = [...elements.getElementsByClassName('n')].map(n => n.innerText);
+    const targetRelease = listOfReleases.filter(list => list.includes(OS));
+    console.log(`target Release: ${targetRelease}`);
+    console.log(`OS: ${OS}`);
+
+    // return if release is matching request url
+    if (targetRelease) {
+      console.log(`getMonoURL: ${url}${targetRelease}`);
+      return `${url}${targetRelease}`;
+    }
+  });
+};
+
+module.exports = getMonoURL;
