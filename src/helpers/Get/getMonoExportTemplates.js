ded3f12d7d96bc7642546c16df10955575eceda8
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2020-02-23 19:05:05 +0200

    Close #128 :tada: create getMonoExportTemplates

---
+++ src/helpers/Get/getMonoExportTemplates.js
@@ -0,0 +1,12 @@
+const { ipcRenderer } = require('electron');
+const path = require('path');
+const process = require('process');
+
+// download export templates of provided specific mono version
+const getMonoExportTemplates = (url, monoExportTemplatesPath, filename) => {
+  const filePath = path.join(process.cwd(), monoExportTemplatesPath, filename);
+
+  ipcRenderer.send('getMonoExportTemplates-request', { url, path: filePath });
+};
+
+module.exports = getMonoExportTemplates;
