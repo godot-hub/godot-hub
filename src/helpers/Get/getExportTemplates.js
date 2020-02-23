ea92c19178da3b35e3bd3603adbe007dcc31adfd
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2020-02-23 18:27:46 +0200

    Close #118 :tada: create getExportTemplates

---
+++ src/helpers/Get/getExportTemplates.js
@@ -0,0 +1,12 @@
+const { ipcRenderer } = require('electron');
+const path = require('path');
+const process = require('process');
+
+// download export templates of provided specific godot version
+const getExportTemplates = (url, exportTemplatesPath, filename) => {
+  const filePath = path.join(process.cwd(), exportTemplatesPath, filename);
+
+  ipcRenderer.send('getExportTemplates-request', { url, path: filePath });
+};
+
+module.exports = getExportTemplates;
