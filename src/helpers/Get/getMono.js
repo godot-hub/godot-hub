eee172d8ea423d944704553ec7ec8bee724a6d27
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2020-02-23 18:47:07 +0200

    Close #116 :tada: create getMono

---
+++ src/helpers/Get/getMono.js
@@ -0,0 +1,12 @@
+const { ipcRenderer } = require('electron');
+const path = require('path');
+const process = require('process');
+
+// download Godot Mono based on provided specific version
+const getMono = (url, monoPath, filename) => {
+  const filePath = path.join(process.cwd(), monoPath, filename);
+
+  ipcRenderer.send('getMono-request', { url, path: filePath });
+};
+
+module.exports = getMono;
