b3b6af9c33320154b1d957dbf8236e5c78a1fe78
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2020-03-01 13:30:38 +0200

    Close #155 :tada: create initEngineDir

---
+++ src/helpers/Init/initEngineDir.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+const process = require('process');
+
+// create a engine directory for a specific release version if it doesn't exist
+const initEngineDir = (version) => {
+  const dirPath = path.join(process.cwd(), 'Godot Hub', version, 'Engine');
+
+  if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath);
+  }
+
+};
+
+module.exports = initEngineDir;
