735004032790fbdf3bc0c9391b1ccd23fdd8b94b
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2020-03-03 22:09:51 +0200

    Close #162 :bug: fix path issues in getMono

--- src/helpers/Get/getMono.js
+++ src/helpers/Get/getMono.js
@@ -6,7 +6,7 @@ const process = require('process');
 const getMono = (url, monoPath, filename) => {
   const filePath = path.join(process.cwd(), monoPath, 'Engine', filename);
 
-  ipcRenderer.send('getMono-request', { url, path: filePath, extractTarget: path.join(process.cwd(), monoPath) });
+  ipcRenderer.send('getMono-request', { url, path: filePath, extractTarget: path.join(process.cwd(), monoPath, 'Engine') });
 };
 
 module.exports = getMono;
