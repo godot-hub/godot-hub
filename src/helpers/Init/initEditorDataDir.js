ed5e9ed661c12d2872179f14b97893654ea1c33e
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2020-03-04 23:57:55 +0200

    Close #164 :pencil2: include monoDir option in initEditorDataDir

--- src/helpers/Init/initEditorDataDir.js
+++ src/helpers/Init/initEditorDataDir.js
@@ -3,11 +3,19 @@ const path = require('path');
 const process = require('process');
 
 // create an editor data directory for a specific release version if it doesn't exist
-const initEditorDataDir = (version) => {
-  const dirPath = path.join(process.cwd(), 'Godot Hub', version, 'Engine', 'editor_data');
+const initEditorDataDir = (version, monoDir = false) => {
+  if (monoDir) {
+    const dirPath = path.join(process.cwd(), 'Godot Hub', version, 'Engine', monoDir, 'editor_data');
 
-  if (!fs.existsSync(dirPath)) {
-    fs.mkdirSync(dirPath);
+    if (!fs.existsSync(dirPath)) {
+      fs.mkdirSync(dirPath);
+    }
+  } else {
+    const dirPath = path.join(process.cwd(), 'Godot Hub', version, 'Engine', 'editor_data');
+
+    if (!fs.existsSync(dirPath)) {
+      fs.mkdirSync(dirPath);
+    }
   }
 };
 
