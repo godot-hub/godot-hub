112a2c81e8816c4c72bb28c5e876f1f1522c47c6
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2020-03-05 00:02:01 +0200

    Close #152 :tada: create installMonoExportTemplates

---
+++ src/helpers/Install/installMonoExportTemplates.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const path = require('path');
+const process = require('process');
+const extract = require('extract-zip');
+const initScFile = require('../Init/initScFile');
+const initEditorDatadir = require('../Init/initEditorDataDir');
+const initTemplatesDir = require('../Init/initTemplatesDir');
+const getFileNameFromURL = require('../URL/getFileNameFromURL');
+const changeFileExtension = require('../Change/changeFileExtension');
+
+// install export templates if its not installed depending on its godot version
+const installMonoExportTemplates = (url, version, monoDir) => {
+  initScFile(version, monoDir);
+  initEditorDatadir(version, monoDir);
+  initTemplatesDir(version, monoDir);
+
+  const dirPath = path.join(process.cwd(), 'Godot Hub', version, 'Engine', monoDir, 'editor_data', 'templates', `${version}.stable.mono`);
+  const monoExportTemplatesFileNameWithoutExtension = getFileNameFromURL(url).slice(0, -4);
+  const monoExportTemplatesDirPath = path.join(process.cwd(), 'Godot Hub', version, 'Engine', monoDir, 'editor_data', 'templates', monoExportTemplatesFileNameWithoutExtension);
+  const monoExportTemplatesPath = path.join(process.cwd(), 'Godot Hub', version, 'Engine', monoDir);
+  const zippedExportTemplatesPath = path.join(process.cwd(), 'Godot Hub', version, 'Engine', monoDir, `${monoExportTemplatesFileNameWithoutExtension}.zip`);
+  const installPath = path.join(process.cwd(), 'Godot Hub', version, 'Engine', monoDir, 'editor_data', 'templates');
+
+  if (!fs.existsSync(dirPath) && !fs.existsSync(monoExportTemplatesDirPath)) {
+    // change file extension
+    changeFileExtension(path.join(monoExportTemplatesPath), monoExportTemplatesFileNameWithoutExtension, '.tpz', '.zip');
+
+    // extract export templates
+    extract(zippedExportTemplatesPath, { dir: installPath }, (err) => {
+      if (err) {
+        console.error(new Error(err));
+      }
+
+      console.log(`${monoExportTemplatesFileNameWithoutExtension}.zip - Unzipped!`);
+
+      // change directory name of installed export templates
+      const currentPath = path.join(installPath, 'templates');
+      const desiredPath = path.join(installPath, `${version}.stable.mono`);
+
+      fs.rename(currentPath, desiredPath, (err) => {
+        if (err) {
+          console.error(new Error(err));
+        }
+
+        console.log(`${currentPath} changed to ${desiredPath}`);
+      });
+
+      changeFileExtension(path.join(monoExportTemplatesPath), monoExportTemplatesFileNameWithoutExtension, '.zip', '.tpz');
+    });
+  }
+};
+
+module.exports = installMonoExportTemplates;
