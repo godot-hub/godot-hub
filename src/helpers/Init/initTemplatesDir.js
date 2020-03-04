95feba4feb5f9fd55b5856e5d1c2fb2feb94b62e
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2020-03-04 23:58:54 +0200

    Close #165 :pencil2: include monoDir option in initTemplatesDir

--- src/helpers/Init/initTemplatesDir.js
+++ src/helpers/Init/initTemplatesDir.js
@@ -3,11 +3,19 @@ const path = require('path');
 const process = require('process');
 
 // create a templates directory for a specific release version if it doesn't exist
-const initTemplatesDir = (version) => {
-  const dirPath = path.join(process.cwd(), 'Godot Hub', version, 'Engine', 'editor_data', 'templates');
+const initTemplatesDir = (version, monoDir = false) => {
+  if (monoDir) {
+    const dirPath = path.join(process.cwd(), 'Godot Hub', version, 'Engine', monoDir, 'editor_data', 'templates');
 
-  if (!fs.existsSync(dirPath)) {
-    fs.mkdirSync(dirPath);
+    if (!fs.existsSync(dirPath)) {
+      fs.mkdirSync(dirPath);
+    }
+  } else {
+    const dirPath = path.join(process.cwd(), 'Godot Hub', version, 'Engine', 'editor_data', 'templates');
+
+    if (!fs.existsSync(dirPath)) {
+      fs.mkdirSync(dirPath);
+    }
   }
 };
 
