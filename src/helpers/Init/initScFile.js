f3113fd28ee60fc9e4e9018e8a6672b425617e2f
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2020-03-04 23:56:48 +0200

    Close #161 :pencil2: include monoDir option in initScFile

--- src/helpers/Init/initScFile.js
+++ src/helpers/Init/initScFile.js
@@ -3,11 +3,19 @@ const path = require('path');
 const process = require('process');
 
 // create a sc file for a specific release version if it doesn't exist
-const initScFile = (version) => {
-  const dirPath = path.join(process.cwd(), 'Godot Hub', version, 'Engine', '_sc_');
+const initScFile = (version, monoDir = false) => {
+  if (monoDir) {
+    const dirPath = path.join(process.cwd(), 'Godot Hub', version, 'Engine', monoDir, '_sc_');
 
-  if (!fs.existsSync(dirPath)) {
-    fs.writeFileSync(dirPath, '');
+    if (!fs.existsSync(dirPath)) {
+      fs.writeFileSync(dirPath, '');
+    }
+  } else {
+    const dirPath = path.join(process.cwd(), 'Godot Hub', version, 'Engine', '_sc_');
+
+    if (!fs.existsSync(dirPath)) {
+      fs.writeFileSync(dirPath, '');
+    }
   }
 };
 
