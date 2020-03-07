861f5f51dab5a15b26b680828ed4b7b127c45d6d
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2020-03-07 17:29:17 +0200

    Close #171 :truck: modify initGodotHubDir to change directory name

--- src/helpers/Init/initGodotHubDir.js
+++ src/helpers/Init/initGodotHubDir.js
@@ -4,7 +4,7 @@ const process = require('process');
 
 // create godot hub directory if it doesn't exist
 const initGodotHubDir = () => {
-  const dirPath = path.join(process.cwd(), 'Godot Hub');
+  const dirPath = path.join(process.cwd(), 'Godot-Hub');
 
   if (!fs.existsSync(dirPath)) {
     fs.mkdirSync(dirPath);
