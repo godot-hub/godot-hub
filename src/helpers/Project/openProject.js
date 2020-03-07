8b8d210e5301b1a7e110891fe928f23787bae962
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2020-03-07 21:45:10 +0200

    Close #175 :ambulance: fix path issues related to changing current directory path

--- src/helpers/Project/openProject.js
+++ src/helpers/Project/openProject.js
@@ -4,12 +4,18 @@ const path = require('path');
 
 // open godot project based on project and godot path
 const openProject = (version, projectPath, godotPath) => {
+  const godotHubPath = process.cwd();
   const parsedProjectPath = path.parse(projectPath).dir;
 
   console.log(process.cwd());
+
+  // change directory to project path
   process.chdir(parsedProjectPath);
+
   console.log(process.cwd());
   console.log(godotPath);
+
+  // open godot project
   execFile(godotPath, ['-e'], (err, stdout, stderr) => {
     if (err) {
       console.error(new Error(err));
@@ -19,6 +25,10 @@ const openProject = (version, projectPath, godotPath) => {
     }
     console.log(stdout);
   });
+
+  // change directory back to godot hub path
+  process.chdir(godotHubPath);
+  console.log(godotHubPath);
 };
 
 module.exports = openProject;
