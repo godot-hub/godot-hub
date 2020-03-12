3ebaaacd27ea586f43b63766fc885946d7daf1ff
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2020-03-12 20:22:57 +0200

    Close #192 :pencil2: modify path in initEngineDir

--- src/helpers/Init/initEngineDir.js
+++ src/helpers/Init/initEngineDir.js
@@ -4,7 +4,7 @@ const process = require('process');
 
 // create a engine directory for a specific release version if it doesn't exist
 const initEngineDir = (version) => {
-  const dirPath = path.join(process.cwd(), 'Godot-Hub', version, 'Engine');
+  const dirPath = path.join(process.cwd(), 'Godot-Hub', 'Releases', version, 'Engine');
 
   if (!fs.existsSync(dirPath)) {
     fs.mkdirSync(dirPath);
