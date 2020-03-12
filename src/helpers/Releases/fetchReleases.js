a32944ceb2c0a147c5e2a6e76c7928da1c0ed423
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2020-03-12 16:27:05 +0200

    Close #184 :building_construction: integrate cacheFetchReleases in fetchReleases

--- src/helpers/Releases/fetchReleases.js
+++ src/helpers/Releases/fetchReleases.js
@@ -1,10 +1,14 @@
+const cacheFetchReleases = require('../Cache/cacheFetchReleases');
+
 // get godot releases
 const fetchReleases = async () => {
   const url = 'https://api.github.com/repos/godotengine/godot/releases';
 
   try {
-    const releases = await fetch(url);
-    return await releases.json();
+    const res = await fetch(url);
+    const releases = await res.json();
+    cacheFetchReleases(releases);
+    return releases;
   } catch (err) {
     console.error(err);
   }
