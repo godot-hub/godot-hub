7e3bbb2e5ff9fd2a5c1ca1379fc7177762a411bd
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2020-03-12 16:31:50 +0200

    Close #188 :building_construction: integrate cacheSortReleases in sortReleases

--- src/helpers/Releases/sortReleases.js
+++ src/helpers/Releases/sortReleases.js
@@ -1,7 +1,9 @@
+const cacheSortReleases = require('../Cache/cacheSortReleases');
+
 // sort releases based on their versions
 const sortReleases = (releases) => {
   try {
-    return releases.reduce((byVersion, release) => {
+    const sortedReleases = releases.reduce((byVersion, release) => {
       const version = release.version[0];
       if (!byVersion[version]) {
         byVersion[version] = [];
@@ -9,6 +11,10 @@ const sortReleases = (releases) => {
       byVersion[version].push(release);
       return byVersion;
     }, {});
+    // cache sorted releases
+    cacheSortReleases(sortedReleases);
+    // return sorted releases
+    return sortedReleases;
   } catch (e) {
     console.error(new Error(e));
   }
