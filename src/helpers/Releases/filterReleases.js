94fb3059388c573483974f309679ea3355bc3116
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2020-03-12 16:30:04 +0200

    Close #186 :building_construction: integrate cacheFilterReleases in filterReleases

--- src/helpers/Releases/filterReleases.js
+++ src/helpers/Releases/filterReleases.js
@@ -1,10 +1,12 @@
+const cacheFilterReleases = require('../Cache/cacheFilterReleases');
+
 // filter releases to only include desired info
 const filterReleases = (rawReleases) => {
   try {
     // don't include version 1.0-stable as there is no download link for it
     const releases = rawReleases.slice(0, rawReleases.length - 1);
     // filter each release to only include the version & url
-    return releases.map(release => {
+    const mappedReleases = releases.map(release => {
       // filtered release values
       const startOfIndex = release.body.indexOf('[Download](') + 11;
       const endOfIndex = release.body.lastIndexOf(')');
@@ -17,6 +19,10 @@ const filterReleases = (rawReleases) => {
         url
       };
     });
+    // cache filter releases
+    cacheFilterReleases(mappedReleases);
+    // return mapped releases
+    return mappedReleases;
   } catch (e) {
     console.error(new Error(e));
   }
