8ccd75b52917e71f9ef03ce4ed11469dbfe203cd
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2020-03-22 21:54:21 +0200

    Close #213 :pencil2: modify initReleasesDir to include path

--- src/helpers/Init/initReleasesDir.js
+++ src/helpers/Init/initReleasesDir.js
@@ -1,10 +1,9 @@
 const fs = require('fs');
 const path = require('path');
-const process = require('process');
 
 // create releases directory if it doesn't exist
-const initReleasesDir = () => {
-  const dirPath = path.join(process.cwd(), 'Godot-Hub', 'Releases');
+const initReleasesDir = (godotHubPath) => {
+  const dirPath = path.join(godotHubPath, 'Releases');
 
   if (!fs.existsSync(dirPath)) {
     fs.mkdirSync(dirPath);
