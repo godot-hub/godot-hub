ec4bffde0a9b36dd5defacf93e98243d53d8b670
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2020-03-22 22:00:21 +0200

    Close #206 :tada: create createGodotHub

---
+++ src/helpers/GodotHub/createGodotHub.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const fs = require('fs');
+const initGodotHubDir = require('../Init/initGodotHubDir');
+const initCacheDir = require('../Init/initCacheDir');
+const initConfigDir = require('../Init/initConfigDir');
+const initReleasesDir = require('../Init/initReleasesDir');
+const initGodotHubConfig = require('../Init/initGodotHubConfig');
+const { ipcRenderer } = require('electron');
+
+// create godot hub directory based on path provided
+const createGodotHub = (createGodotHubPath) => {
+  // path to check if there is already a godot hub directory
+  const godotHubPath = path.join(createGodotHubPath, 'Godot-Hub');
+
+  if (!fs.existsSync(godotHubPath)) {
+    initGodotHubDir(createGodotHubPath);
+    initCacheDir(godotHubPath);
+    initConfigDir(godotHubPath);
+    initReleasesDir(godotHubPath);
+    initGodotHubConfig(godotHubPath);
+    ipcRenderer.send('navigate', { filePath: './src/index.html' });
+  }
+};
+
+module.exports = createGodotHub;
