91c2d4edb1ff38ef38725a9b7f325fe67d1e083d
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2020-03-29 10:58:40 +0200

    Close #258 :pencil2: modify filterReleases to include mono versions

--- src/helpers/Releases/filterReleases.js
+++ src/helpers/Releases/filterReleases.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const cacheFilterReleases = require('../Cache/cacheFilterReleases');
 
 // filter releases to only include desired info
@@ -5,26 +6,61 @@ const filterReleases = (rawReleases, godotHubPath) => {
   try {
     // don't include version 1.0-stable as there is no download link for it
     const releases = rawReleases.slice(0, rawReleases.length - 1);
-    // filter each release to only include the version & url
-    const mappedReleases = releases.map(release => {
-      // filtered release values
-      const startOfIndex = release.body.indexOf('[Download](') + 11;
-      const endOfIndex = release.body.lastIndexOf(')');
 
-      const name = release.name.slice(0, release.name.indexOf('-'));
-      const version = release.name;
-      const url = release.body.slice(startOfIndex, endOfIndex);
+    // keep track of filtered releases
+    let filteredReleases = [];
 
-      return {
-        name,
-        version,
-        url
-      };
-    });
-    // cache filter releases
-    cacheFilterReleases(mappedReleases, godotHubPath);
-    // return mapped releases
-    return mappedReleases;
+    // loop over releases to filter out required data
+    for (let i = 0; i < releases.length; i++) {
+      const startOfIndex = releases[i].body.indexOf('[Download](') + 11;
+      const endOfIndex = releases[i].body.lastIndexOf(')');
+
+      let name;
+      let version;
+      let url;
+
+      // include mono if last index version value is at least 3
+      if (releases[i].name[0] >= 3) {
+        // normal releases
+        name = releases[i].name.slice(0, releases[i].name.indexOf('-'));
+        version = releases[i].name;
+        url = releases[i].body.slice(startOfIndex, endOfIndex);
+
+        filteredReleases.push({
+          name,
+          version,
+          url
+        });
+
+        // mono releases
+        const monoName = `${releases[i].name.slice(0, releases[i].name.indexOf('-'))}-mono`;
+        const monoVersion = `${releases[i].name}-mono`;
+        const monoUrl = path.join(releases[i].body.slice(startOfIndex, endOfIndex), 'mono');
+
+        const monoReleases = {
+          name: monoName,
+          version: monoVersion,
+          url: monoUrl
+        };
+
+        filteredReleases.push(monoReleases);
+      } else {
+        name = releases[i].name.slice(0, releases[i].name.indexOf('-'));
+        version = releases[i].name;
+        url = releases[i].body.slice(startOfIndex, endOfIndex);
+
+        filteredReleases.push({
+          name,
+          version,
+          url
+        });
+      }
+    }
+
+    // cache filtered and mono releases
+    cacheFilterReleases(filteredReleases, godotHubPath);
+    // return filtered and mono releases
+    return filteredReleases;
   } catch (e) {
     console.error(new Error(e));
   }
