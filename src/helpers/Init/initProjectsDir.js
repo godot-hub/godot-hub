593260d9f61d482ea9a3eb73a587b6ff83219b2d
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2020-04-01 23:42:27 +0200

    Close #269 :pencil2: include godotHubPath in initProjectsDir

--- src/helpers/Init/initProjectsDir.js
+++ src/helpers/Init/initProjectsDir.js
@@ -3,8 +3,8 @@ const path = require('path');
 const process = require('process');
 
 // create a projects directory for a specific release version if it doesn't exist
-const initProjectsDir = (version) => {
-  const dirPath = path.join(process.cwd(), 'Godot-Hub', 'Releases', version, 'Projects');
+const initProjectsDir = (godotHubPath, version) => {
+  const dirPath = path.join(godotHubPath, 'Releases', version, 'Projects');
 
   if (!fs.existsSync(dirPath)) {
     fs.mkdirSync(dirPath);
