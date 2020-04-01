a3810e121dcd918502812bd043ed2c6104b7f1f8
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2020-04-01 23:41:11 +0200

    Close #268 :pencil2: include godotHubPath in initEngineDir

--- src/helpers/Init/initEngineDir.js
+++ src/helpers/Init/initEngineDir.js
@@ -3,8 +3,8 @@ const path = require('path');
 const process = require('process');
 
 // create a engine directory for a specific release version if it doesn't exist
-const initEngineDir = (version) => {
-  const dirPath = path.join(process.cwd(), 'Godot-Hub', 'Releases', version, 'Engine');
+const initEngineDir = (godotHubPath, version) => {
+  const dirPath = path.join(godotHubPath, 'Releases', version, 'Engine');
 
   if (!fs.existsSync(dirPath)) {
     fs.mkdirSync(dirPath);
