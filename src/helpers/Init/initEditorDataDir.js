7dcb9f5a3cd39742c810fbdf01f3af3fab2e4e6c
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2020-04-01 23:39:24 +0200

    Close #266 :pencil2: include godotHubPath in initEditorDataDir

--- src/helpers/Init/initEditorDataDir.js
+++ src/helpers/Init/initEditorDataDir.js
@@ -1,21 +1,12 @@
 const fs = require('fs');
 const path = require('path');
-const process = require('process');
 
 // create an editor data directory for a specific release version if it doesn't exist
-const initEditorDataDir = (version, monoDir = false) => {
-  if (monoDir) {
-    const dirPath = path.join(process.cwd(), 'Godot-Hub', 'Releases', version, 'Engine', monoDir, 'editor_data');
+const initEditorDataDir = (godotHubPath, version) => {
+  const dirPath = path.join(godotHubPath, 'Releases', version, 'Engine', 'editor_data');
 
-    if (!fs.existsSync(dirPath)) {
-      fs.mkdirSync(dirPath);
-    }
-  } else {
-    const dirPath = path.join(process.cwd(), 'Godot-Hub', 'Releases', version, 'Engine', 'editor_data');
-
-    if (!fs.existsSync(dirPath)) {
-      fs.mkdirSync(dirPath);
-    }
+  if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath);
   }
 };
 
