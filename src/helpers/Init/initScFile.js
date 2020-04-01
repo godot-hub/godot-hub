d33dc257a9226549f4d2826b142a6c49452a791a
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2020-04-01 23:36:38 +0200

    Close #264 :pencil2: include godotHubPath in initScFile

--- src/helpers/Init/initScFile.js
+++ src/helpers/Init/initScFile.js
@@ -1,21 +1,12 @@
 const fs = require('fs');
 const path = require('path');
-const process = require('process');
 
 // create a sc file for a specific release version if it doesn't exist
-const initScFile = (version, monoDir = false) => {
-  if (monoDir) {
-    const dirPath = path.join(process.cwd(), 'Godot-Hub', 'Releases', version, 'Engine', monoDir, '_sc_');
+const initScFile = (godotHubPath, version) => {
+  const dirPath = path.join(godotHubPath, 'Releases', version, 'Engine', '_sc_');
 
-    if (!fs.existsSync(dirPath)) {
-      fs.writeFileSync(dirPath, '');
-    }
-  } else {
-    const dirPath = path.join(process.cwd(), 'Godot-Hub', 'Releases', version, 'Engine', '_sc_');
-
-    if (!fs.existsSync(dirPath)) {
-      fs.writeFileSync(dirPath, '');
-    }
+  if (!fs.existsSync(dirPath)) {
+    fs.writeFileSync(dirPath, '');
   }
 };
 
