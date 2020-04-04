12d0352b0cdd3f429f8aedd051fe3e3b602647b2
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2020-04-04 19:34:03 +0200

    Close #276 :pencil2: include godotHubPath in getGodot

--- src/helpers/Get/getGodot.js
+++ src/helpers/Get/getGodot.js
@@ -1,7 +1,5 @@
 const { ipcRenderer } = require('electron');
 const path = require('path');
-const fs = require('fs');
-const process = require('process');
 const initScFile = require('../Init/initScFile');
 const initReleaseDir = require('../Init/initReleaseDir');
 const initProjectsDir = require('../Init/initProjectsDir');
@@ -10,11 +8,7 @@ const initEditorDataDir = require('../Init/initEditorDataDir');
 const initTemplatesDir = require('../Init/initTemplatesDir');
 
 // download Godot based on provided specific version
-const getGodot = (url, godotPath, filename, version) => {
-  // godot hub path
-  const godotHubConfigPath = path.join(process.cwd(), 'godot-hub.json');
-  const godotHubPath = JSON.parse(fs.readFileSync(godotHubConfigPath)).godotHubPath;
-
+const getGodot = (url, godotHubPath, filename, version) => {
   // init required directories
   initReleaseDir(godotHubPath, version);
   initEngineDir(godotHubPath, version);
@@ -23,11 +17,11 @@ const getGodot = (url, godotPath, filename, version) => {
   initTemplatesDir(godotHubPath, version);
 
   // get godot version
-  const filePath = path.join(godotPath, 'Releases', version, 'Engine', filename);
+  const filePath = path.join(godotHubPath, 'Releases', version, 'Engine', filename);
 
-  ipcRenderer.send('getGodot-request', { url, path: filePath, extractTarget: path.join(godotPath, 'Releases', version, 'Engine') });
+  ipcRenderer.send('getGodot-request', { url, path: filePath, extractTarget: path.join(godotHubPath, 'Releases', version, 'Engine') });
   ipcRenderer.on('getGodot-Done', () => {
-    initScFile(godotPath, version);
+    initScFile(godotHubPath, version);
     console.log('getGodot - DONE');
   });
 };
