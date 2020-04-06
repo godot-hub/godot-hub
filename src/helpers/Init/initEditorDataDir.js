0046e1e63e8e0e4764a511dcc0bb93ebaf92b5a5
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2020-04-07 00:29:48 +0200

    Close #292 :ambulance: fix incorrect path for initEditorDataDir

--- src/helpers/Init/initEditorDataDir.js
+++ src/helpers/Init/initEditorDataDir.js
@@ -2,8 +2,14 @@ const fs = require('fs');
 const path = require('path');
 
 // create an editor data directory for a specific release version if it doesn't exist
-const initEditorDataDir = (godotHubPath, version) => {
-  const dirPath = path.join(godotHubPath, 'Releases', version, 'Engine', 'editor_data');
+const initEditorDataDir = (godotHubPath, version, mono = false, monoDir = '') => {
+  let dirPath;
+
+  if (mono) {
+    dirPath = path.join(godotHubPath, 'Releases', version, 'Engine', monoDir, 'editor_data');
+  } else {
+    dirPath = path.join(godotHubPath, 'Releases', version, 'Engine', 'editor_data');
+  }
 
   if (!fs.existsSync(dirPath)) {
     fs.mkdirSync(dirPath);
