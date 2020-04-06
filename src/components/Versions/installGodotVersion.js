540748ea2ecbcf60eb971e9a5bf9f7ac7cbcdf18
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2020-04-06 22:47:34 +0200

    Close #282 :sparkles: show download progress in versions component when downloading godot/mono

--- src/components/Versions/installGodotVersion.js
+++ src/components/Versions/installGodotVersion.js
@@ -1,3 +1,4 @@
+const { ipcRenderer } = require('electron');
 const getGodotURL = require('../../helpers/URL/getGodotURL');
 const getMonoURL = require('../../helpers/URL/getMonoURL');
 const getOSinfo = require('../../helpers/getOSinfo');
@@ -8,10 +9,62 @@ const installGodotVersion = (parent, type, url, version, godotHubPath) => {
     const OS = getOSinfo(true);
 
     getMonoURL(url, OS, version, godotHubPath);
+
+    const progressElement = parent.querySelector('.progress');
+    const installElement = parent.querySelector('.install');
+
+    if (!progressElement) {
+      // show progress element
+      const progress = document.createElement('p');
+      const progressText = document.createTextNode('0% - 0/0 MB');
+      progress.appendChild(progressText);
+
+      parent.removeChild(installElement);
+      parent.appendChild(progress);
+      progress.setAttribute('class', 'progress');
+
+      ipcRenderer.on(`${version}-progress`, (event, arg) => {
+        const { percentage, total, current, totalMB, currentMB } = arg;
+        console.log(`
+          percentage: ${percentage}\n
+          total: ${total}\n
+          current: ${current}\n
+          totalMB: ${totalMB}MB\n
+          currentMB: ${currentMB}MB\n
+        `);
+        progress.textContent = ` ${percentage}% - ${currentMB}/${totalMB} MB`;
+      });
+    }
   } else {
     const OS = getOSinfo();
 
     getGodotURL(url, OS, version, godotHubPath);
+
+    const progressElement = parent.querySelector('.progress');
+    const installElement = parent.querySelector('.install');
+
+    if (!progressElement) {
+      // show progress element
+      const progress = document.createElement('p');
+      const progressText = document.createTextNode('0% - 0/0 MB');
+      progress.appendChild(progressText);
+
+      parent.removeChild(installElement);
+      parent.appendChild(progress);
+      progress.setAttribute('class', 'progress');
+
+      ipcRenderer.on(`${version}-progress`, (event, arg) => {
+        const { percentage, total, current, totalMB, currentMB } = arg;
+        console.log(`
+          percentage: ${percentage}\n
+          total: ${total}\n
+          current: ${current}\n
+          totalMB: ${totalMB}MB\n
+          currentMB: ${currentMB}MB\n
+        `);
+        progress.textContent = ` ${percentage}% - ${currentMB}/${totalMB} MB`;
+      });
+    }
   }
 };
 
