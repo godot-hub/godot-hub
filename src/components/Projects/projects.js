ac3ca9a186c216c3b9a59c83a87bd25eaf393968
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2020-04-12 01:55:32 +0200

    Close #311 :sparkles: render current projects in projects component

--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -1,6 +1,16 @@
 const { ipcRenderer } = require('electron');
+const path = require('path');
+const fs = require('fs');
 const back = document.querySelector('#back');
 
 back.addEventListener('click', () => {
   ipcRenderer.send('navigate', { filePath: './src/components/Index/index.html' });
 });
+
+// godot hub path
+const godotHubConfigPath = path.join(process.cwd(), 'godot-hub.json');
+const godotHubPath = JSON.parse(fs.readFileSync(godotHubConfigPath)).godotHubPath;
+
+// render current projects
+const renderProjects = require('./renderProjects');
+renderProjects(godotHubPath);
