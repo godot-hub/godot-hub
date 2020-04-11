c634102f17220265f6ba12c10b170a9047a56a32
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2020-04-12 01:32:17 +0200

    Close #305 :tada: create renderProjects

---
+++ src/components/Projects/renderProjects.js
@@ -0,0 +1,38 @@
+const getCurrentProjects = require('../../helpers/Project/getCurrentProjects');
+
+// render projects in projects view based on the current valid projects
+const renderProjects = (godotHubPath) => {
+  // get current projects
+  const currentProjectsList = getCurrentProjects(godotHubPath);
+
+  const projectsList = document.querySelector('#projects-list');
+
+  currentProjectsList.forEach(currentProjects => {
+    if (currentProjects.length > 0) {
+      currentProjects.forEach(currentProject => {
+        const { name, version, projectPath, filePath, godotPath } = currentProject;
+
+        const project = `<li 
+            class="project"
+            data-name="${name}"
+            data-version="${version}"
+            data-project-path="${projectPath}"
+            data-file-path="${filePath}"
+            data-godot-path="${godotPath}"
+          >
+          <h2>${name}</h2>
+          <h3>${version}</h3>
+          <span class="project-options">
+            <p class="launch">Launch</p>
+            <p class="edit">Edit</p>
+            <p class="delete">Delete</p>
+          </span>
+        </li>`;
+
+        projectsList.insertAdjacentHTML('beforeend', project);
+      });
+    }
+  });
+};
+
+module.exports = renderProjects;
