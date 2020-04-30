0b27fb0d1299e96d6844a11692c26075bccb6f43
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2020-04-30 22:26:39 +0200

    Close #335 :sparkles: apply validation when importing project

--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -3,7 +3,7 @@ const dialog = require('electron').remote.dialog;
 const path = require('path');
 const fs = require('fs');
 const back = document.querySelector('#back');
-const getCurrentProjects = require('../../helpers/Project/getCurrentProjects');
+const validateImportProject = require('./validateImportProject');
 
 back.addEventListener('click', () => {
   ipcRenderer.send('navigate', { filePath: './src/components/Index/index.html' });
@@ -218,95 +218,15 @@ importProjectbutton.addEventListener('click', () => {
       properties: ['openDirectory']
     });
 
-    if (inputPath.filePaths[0]) {
-      // reset project path warning element
-      if (projectPathWarningElement.textContent.length > 0) {
-        projectPathWarningElement.textContent = '';
-      }
+    const args = {
+      godotHubPath,
+      inputPath,
+      projectPathWarningElement,
+      projectPathInput,
+      projectGodotVersionInput
+    };
 
-      // reset path input value
-      if (projectPathInput.value.length > 0) {
-        projectPathInput.value = '';
-      }
-
-      const godotProjectPath = inputPath.filePaths[0];
-      const godotProjectName = path.parse(inputPath.filePaths[0]).name;
-
-      console.log(godotProjectPath);
-
-      const godotProjectSubDirs = fs.readdirSync(godotProjectPath);
-
-      console.log(godotProjectSubDirs);
-      console.log(godotProjectName);
-
-      const currentProjects = getCurrentProjects(godotHubPath).flat();
-      const currentProjectsNames = currentProjects.map(project => project.name);
-
-      console.log(currentProjects);
-      console.log(currentProjectsNames);
-
-      // check if the selected project is a valid godot project
-
-      // check if the selected project doesn't exist
-      if (currentProjectsNames.includes(godotProjectName)) {
-        const currentProject = currentProjects.filter(project => {
-          return project.name === godotProjectName;
-        });
-
-        const isNotValidProject = currentProject.map(project => {
-          console.log(project.version);
-          console.log(projectGodotVersionInput.value);
-          if (parseInt(project.version) === parseInt(projectGodotVersionInput.value)) {
-            return true;
-          }
-        });
-
-        console.log(isNotValidProject);
-
-        // import project if project name exists but versions don't match
-        if (!isNotValidProject.includes(true)) {
-          // make project path show if its valid
-          console.log('allowed');
-          // allow only valid godot projects with compatiable versions based on their config file
-
-          // reject if the path chosen is not a valid godot project
-          if (!godotProjectSubDirs.includes('project.godot') && !godotProjectSubDirs.includes('engine.cfg')) {
-            if (projectPathWarningElement.textContent !== 'not a valid godot project') {
-              projectPathWarningElement.textContent = 'not a valid godot project';
-            }
-          } else {
-            if (godotProjectSubDirs.includes('project.godot') && parseInt(projectGodotVersionInput.value[0]) >= 3) {
-              projectPathInput.value = godotProjectPath;
-              console.log(projectGodotVersionInput.value);
-            } else if (godotProjectSubDirs.includes('engine.cfg') && parseInt(projectGodotVersionInput.value[0]) < 3) {
-              projectPathInput.value = godotProjectPath;
-              console.log(projectGodotVersionInput.value);
-            } else {
-              projectPathWarningElement.textContent = 'project version doesn\'t match';
-            }
-          }
-        } else {
-          projectPathWarningElement.textContent = 'project already exists with the current version';
-        }
-      } else {
-        // reject if the path chosen is not a valid godot project
-        if (!godotProjectSubDirs.includes('project.godot') && !godotProjectSubDirs.includes('engine.cfg')) {
-          if (projectPathWarningElement.textContent !== 'not a valid godot project') {
-            projectPathWarningElement.textContent = 'not a valid godot project';
-          }
-        } else {
-          if (godotProjectSubDirs.includes('project.godot') && parseInt(projectGodotVersionInput.value[0]) >= 3) {
-            projectPathInput.value = godotProjectPath;
-            console.log(projectGodotVersionInput.value);
-          } else if (godotProjectSubDirs.includes('engine.cfg') && parseInt(projectGodotVersionInput.value[0]) < 3) {
-            projectPathInput.value = godotProjectPath;
-            console.log(projectGodotVersionInput.value);
-          } else {
-            projectPathWarningElement.textContent = 'project version doesn\'t match';
-          }
-        }
-      }
-    }
+    validateImportProject(args);
   });
 
   const versionParent = document.createElement('div');
