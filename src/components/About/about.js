66b84d53b8e03ce3819fe402210e4769af1e5571
Mrwan Ashraf <mrwan.ashraf94@gmail.com>
2020-05-10 00:52:18 +0200

    Close #344 :pencil2: modify about component to include basic info

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -1,6 +1,11 @@
 const { ipcRenderer } = require('electron');
+const { app } = require('electron').remote;
 const back = document.querySelector('#back');
 
 back.addEventListener('click', () => {
   ipcRenderer.send('navigate', { filePath: './src/components/Index/index.html' });
 });
+
+// get current app version
+const version = document.querySelector('#version');
+version.textContent = app.getVersion();
